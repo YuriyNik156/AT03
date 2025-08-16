cdb211a0b6ebca32a7fce9f3dffc3b517dd2a743
YuriyNik156 <yuriy.nik156@gmail.com>
2025-08-16 15:59:38 +0300

    add def test_get_weather_with_error

--- lesson/test.py
+++ lesson/test.py
@@ -18,3 +18,14 @@ def test_get_weather(mocker):
         'weather' : [{'description': 'clear sky'}],
         'main' : {'temp' : 273.15}
     }
+
+def test_get_weather_with_error(mocker):
+    mock_get = mocker.patch('main.requests.get')
+    mock_get.return_value.status_code = 404
+
+    api_key = '13e2e91ca5585170bfc25294bde29a16'
+    city = 'London'
+
+    weather_data = get_weather(api_key, city)
+
+    assert weather_data == None
