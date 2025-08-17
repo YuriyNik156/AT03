62f429c0e7ebd31af01612589aacef5d2fa9c91f
YuriyNik156 <yuriy.nik156@gmail.com>
2025-08-17 13:01:00 +0300

    add pytest.mark.parametrize

--- hometask/test_the_cat_api.py
+++ hometask/test_the_cat_api.py
@@ -1,25 +1,27 @@
 import pytest
 from the_cat_api import get_image_api_cat
 
-def test_get_image_api_cat(mocker):
+@pytest.mark.parametrize(
+    "status_code, json_return_value, expected",
+    [
+        (
+            200,
+            [{"id": "34578", "url": "https://cdn2.thecatapi.com/images/34578.jpg"}],
+            "https://cdn2.thecatapi.com/images/34578.jpg",
+        ),
+        (
+            404,
+            None,
+            None,
+        ),
+    ],
+)
+def test_get_image_api_cat(mocker, status_code, json_return_value, expected):
     mock_get = mocker.patch("the_cat_api.requests.get")
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.json.return_value = [
-        {
-        'id' : '34578',
-        'url' : 'https://cdn2.thecatapi.com/images/34578.jpg'
-    }
-    ]
+    mock_get.return_value.status_code = status_code
+    mock_get.return_value.json.return_value = json_return_value
 
     cat_url = get_image_api_cat()
 
-    assert cat_url == 'https://cdn2.thecatapi.com/images/34578.jpg'
-
-def test_get_image_api_cat_with_error(mocker):
-    mock_get = mocker.patch("the_cat_api.requests.get")
-    mock_get.return_value.status_code = 404
-
-    cat_url = get_image_api_cat()
-
-    assert cat_url is None
+    assert cat_url == expected
 
